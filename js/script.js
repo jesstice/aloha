7d69142f5cfbe0028219aa4d7591d4fc9ca4bdf6
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-04-23 01:09:02 -0700

    added form alert dialog on js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ $(function() {
   // FLICKITY SLIDER
 	$('.main-carousel').flickity({
   // options
-  cellAlign: 'left',
-  contain: true
-});
+  	cellAlign: 'left',
+  	contain: true
+	});
 
   // FORM SUBMIT FUN
-
+	$('.subscribe').on('submit', function(event) {
+  	event.preventDefault();
+    
+    if ( $('#email-input').val() == '' ) {
+      alert('You forgot to enter your email! Try again!');  
+    } else {
+      alert('Thanks for signing up! We promise not to spam you...(too much!)');  
+    }; 
+  });
+  
 });
