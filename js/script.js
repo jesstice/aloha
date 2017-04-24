999684fc55905e042e18352308a5538a2d849314
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-04-23 23:08:10 -0700

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
 $(function() {
 
   // SMOOOOOTH SCROLLING
+	// Select all links with hashes
+	$('a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function(event) {
+			// On-page links
+			if (
+				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+				&& 
+				location.hostname == this.hostname
+			) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000, function() {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) { // Checking if the target was focused
+							return false;
+						} else {
+							$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						};
+					});
+				}
+			}
+		});
 
   // FLICKITY SLIDER
 	$('.main-carousel').flickity({
@@ -14,9 +50,9 @@ $(function() {
   	event.preventDefault();
     
     if ( $('#email-input').val() == '' ) {
-      alert('You forgot to enter your email! Try again!');  
+      alert('Oops! You forgot to enter your email. Try again!');  
     } else {
-      alert('Thanks for signing up! We promise not to spam you...(too much!)');  
+      alert('Thanks for subscribing! We promise not to spam you!');  
     }; 
   });
   
